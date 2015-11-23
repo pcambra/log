3287cef255bdc0a19bc78fed5dd6d82963031780
zaporylie <jakub@nymedia.no>
2015-11-23 20:29:49 +0100

    Add field permission

--- src/LogAccessControlHandler.php
+++ src/LogAccessControlHandler.php
@@ -10,6 +10,8 @@ namespace Drupal\log;
 use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Entity\EntityAccessControlHandler;
 use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Field\FieldDefinitionInterface;
+use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Session\AccountInterface;
 
 /**
@@ -47,6 +49,26 @@ class LogAccessControlHandler extends EntityAccessControlHandler {
    */
   protected function checkCreateAccess(AccountInterface $account, array $context, $entity_bundle = NULL) {
     return AccessResult::allowedIfHasPermission($account, "create $entity_bundle log");
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function checkFieldAccess($operation, FieldDefinitionInterface $field_definition, AccountInterface $account, FieldItemListInterface $items = NULL) {
+    // Only users with the administer nodes permission can edit administrative
+    // fields.
+    $administrative_fields = array('user_id', 'created');
+    if ($operation == 'edit' && in_array($field_definition->getName(), $administrative_fields, TRUE)) {
+      return AccessResult::allowedIfHasPermission($account, 'administer logs');
+    }
+
+    // No user can change read only fields.
+    $read_only_fields = array('revision_timestamp', 'revision_uid');
+    if ($operation == 'edit' && in_array($field_definition->getName(), $read_only_fields, TRUE)) {
+      return AccessResult::forbidden();
+    }
+
+    return parent::checkFieldAccess($operation, $field_definition, $account, $items);
   }
 
 }

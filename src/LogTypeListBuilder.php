e8810cb7747935794a00bf5fd2a647789dbb8058
Pedro Cambra <pedro.cambra@gmail.com>
2019-12-08 18:25:22 +0100

    Added operations to the type builder

--- src/LogTypeListBuilder.php
+++ src/LogTypeListBuilder.php
@@ -4,6 +4,7 @@ namespace Drupal\log;
 
 use Drupal\Core\Config\Entity\ConfigEntityListBuilder;
 use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Url;
 
 /**
  * Provides a listing of Log type entities.
@@ -27,6 +28,30 @@ class LogTypeListBuilder extends ConfigEntityListBuilder {
     $row['id'] = $entity->id();
     // You probably want a few more properties here...
     return $row + parent::buildRow($entity);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getDefaultOperations(EntityInterface $entity) {
+    $operations = parent::getDefaultOperations($entity);
+    // Place the edit operation after the operations added by field_ui.module
+    // which have the weights 15, 20, 25.
+    if (isset($operations['edit'])) {
+      $operations['edit']['weight'] = 30;
+    }
+    return $operations;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function render() {
+    $build = parent::render();
+    $build['table']['#empty'] = $this->t('No log types available. <a href=":link">Add log type</a>.', [
+      ':link' => Url::fromRoute('entity.log_type.add_form')->toString(),
+    ]);
+    return $build;
   }
 
 }
